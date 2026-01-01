3a7838ff3029207468998cd33e03012982aa925f
lacko186 <laszlobogdan0619@gmail.com>
2026-01-01 17:03:16 +0100

    helyadatok lekérdezése helynév alapján vagy szálességi hosszúsági fokok alapján,46.352821, 17.795034

---
+++ location.py
@@ -0,0 +1,27 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+
+
+geolocator = Nominatim(user_agent="Helykereso")
+location = geolocator.geocode("Barcs, Hungary")
+
+print(f'{location.address} \n')
+print(f'{(location.latitude, location.longitude)}\n')
+print(f'{location.raw}\n')
+
+
+
+
+kiindulópont = ['46.072046', '18.231495']
+kiinduloponneve = 'Pécs'
+célpont = geolocator.geocode("46.352821, 17.795034")
+célpontneve = 'Kaposvár'
+
+
+
+distance = geodesic((kiindulópont[0], kiindulópont[1]), (célpont.latitude, célpont.longitude)).km
+
+print(f' {kiinduloponneve} és {célpontneve} közötti távolság: {distance} km Légvonalban.')
+
+
+
